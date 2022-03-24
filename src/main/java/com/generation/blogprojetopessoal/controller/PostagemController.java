8613e08ea62cc0930effcf5c50c91d088a85e48d
Rosi <rosy.marttins@gmail.com>
2022-03-24 18:17:01 -0300

    CRUD 1

---
+++ src/main/java/com/generation/blogprojetopessoal/controller/PostagemController.java
@@ -0,0 +1,28 @@
+package com.generation.blogprojetopessoal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogprojetopessoal.model.Postagem;
+import com.generation.blogprojetopessoal.repository.PostagemRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+		
+	}
+}
