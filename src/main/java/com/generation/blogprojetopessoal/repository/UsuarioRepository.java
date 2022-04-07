d4cb4caabe8fe79cf68e2cc1048b9d85c4d4f07e
Rosi <rosy.marttins@gmail.com>
2022-04-07 14:52:55 -0300

    adicionado o test

--- src/main/java/com/generation/blogprojetopessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogprojetopessoal/repository/UsuarioRepository.java
@@ -1,16 +1,19 @@
 package com.generation.blogprojetopessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
-
-import com.generation.blogprojetopessoal.model.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.generation.blogprojetopessoal.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	
 	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 
 }
--- src/main/java/com/generation/blogprojetopessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogprojetopessoal/service/UsuarioService.java
@@ -3,16 +3,14 @@ package com.generation.blogprojetopessoal.service;
 import java.nio.charset.Charset;
 import java.util.Optional;
 
-import org.apache.commons.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.generation.blogprojetopessoal.model.Usuario;
 import com.generation.blogprojetopessoal.model.UsuarioLogin;
 import com.generation.blogprojetopessoal.repository.UsuarioRepository;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
@@ -34,15 +32,10 @@ public class UsuarioService {
 	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
 
 		if (usuarioRepository.findById(usuario.getId()).isPresent()) {
-			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
-
-			if ((buscaUsuario.isPresent()) && (buscaUsuario.get().getId() != usuario.getId()))
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O usuário já existe!", null);
 
 			usuario.setSenha(criptografarSenha(usuario.getSenha()));
-			
 
-			return Optional.of(usuarioRepository.save(usuario));
+			return Optional.ofNullable(usuarioRepository.save(usuario));
 
 		}
 
@@ -52,26 +45,29 @@ public class UsuarioService {
 
 	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
 
+		
 		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
 
 		if (usuario.isPresent()) {
 
-			if (compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
+		if (compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
 
+				
 				usuarioLogin.get().setId(usuario.get().getId());
 				usuarioLogin.get().setNome(usuario.get().getNome());
 				usuarioLogin.get().setFoto(usuario.get().getFoto());
-				usuarioLogin.get()
-						.setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
+				usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
 				usuarioLogin.get().setSenha(usuario.get().getSenha());
 
+				
 				return usuarioLogin;
 
 			}
-		}
-
+		}	
+		
+		
 		return Optional.empty();
-
+		
 	}
 
 	private String criptografarSenha(String senha) {
