1246a4052bd540d16d0cf59ebfd360dea1dbeeae
Rosi <rosy.marttins@gmail.com>
2022-04-11 14:24:54 -0300

    deploy do blog projeto pessoal

--- src/main/java/com/generation/blogprojetopessoal/model/Postagem.java
+++ src/main/java/com/generation/blogprojetopessoal/model/Postagem.java
@@ -46,6 +46,14 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Usuario usuario;
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	public Long getId() {
 		return id;
 	}
